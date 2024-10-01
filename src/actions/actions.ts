7021ed29ee651d0f01d433ea71bb6b92829e7ae0
kb603 <kojoaddobaffour@gmail.com>
2024-10-01 16:46:18 +0000

    updates

---
+++ src/actions/actions.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import prisma from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+// CREATE A POST
+
+export async function createPost(formData: FormData) {
+  await prisma.post.create({
+    data: {
+      title: formData.get("title") as string,
+      slug: (formData.get("title") as string)
+        .replace(/\s+/g, "-")
+        .toLowerCase(),
+      content: formData.get("content") as string,
+    },
+  });
+  revalidatePath("/posts");
+}
+
+// EDIT A POST
+
+export async function editPost(formData: FormData, id: string) {
+  await prisma.post.update({
+    where: { id },
+    data: {
+      title: formData.get("title") as string,
+      slug: (formData.get("title") as string)
+        .replace(/\s+/g, "-")
+        .toLowerCase(),
+      content: formData.get("content") as string,
+    },
+  });
+}
+
+export async function deletePost(id: string) {
+  await prisma.post.delete({
+    where: { id },
+  });
+}
