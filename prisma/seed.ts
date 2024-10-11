21116e859096579412ef2aec19df5d2ef1b05207
kb603 <kojoaddobaffour@gmail.com>
2024-10-11 19:36:38 +0000

    updates

---
+++ prisma/seed.ts
@@ -0,0 +1,43 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const initialPosts: Prisma.PostCreateInput[] = [
+  {
+    title: "Post 1",
+    slug: "post-1",
+    content: "Content 1",
+    author: {
+      connectOrCreate: {
+        where: {
+          email: "john@gmail.com",
+        },
+        create: {
+          email: "john@gmail.com",
+          hashedPasssword: "3456789dftghj",
+        },
+      },
+    },
+  },
+];
+
+async function main() {
+  console.log("Start seeding ...");
+
+  for (const post of initialPosts) {
+    const newPost = await prisma.post.create({
+      data: post,
+    });
+    console.log(`Created post with id: ${newPost.id}`);
+  }
+
+  console.log("Seeding finished.");
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,20 +1,35 @@
 "use server";
 
 import prisma from "@/lib/db";
+import { Prisma } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
 // CREATE A POST
 
 export async function createPost(formData: FormData) {
-  await prisma.post.create({
-    data: {
-      title: formData.get("title") as string,
-      slug: (formData.get("title") as string)
-        .replace(/\s+/g, "-")
-        .toLowerCase(),
-      content: formData.get("content") as string,
-    },
-  });
+  try {
+    await prisma.post.create({
+      data: {
+        title: formData.get("title") as string,
+        slug: (formData.get("title") as string)
+          .replace(/\s+/g, "-")
+          .toLowerCase(),
+        content: formData.get("content") as string,
+        author: {
+          connect: { id: formData.get("authorId") as string },
+        },
+      },
+    });
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        console.log(
+          "There is a unique constraint violation, a new user cannot be created with this email"
+        );
+      }
+    }
+  }
+
   revalidatePath("/posts");
 }
 
